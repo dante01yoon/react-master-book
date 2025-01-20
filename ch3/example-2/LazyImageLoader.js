97c1a3542fbab16df6c8c2e4563225b43f44baa4
dante <dante@danteui-MacBookAir.local>
2025-01-20 20:53:50 +0900

    feat: apply lazy image loader spinner

--- ch3/example-2/LazyImageLoader.js
+++ ch3/example-2/LazyImageLoader.js
@@ -1,4 +1,4 @@
-class LazyImageLoader extends HTMLElement {
+class LazyImageLoader extends HTMLElement { // HTMLElement에서 제공하는 인터페이스를 사용하기 위해 상속했습니다.
   constructor() {
     super();
     this._image = document.createElement('img');
@@ -8,11 +8,11 @@ class LazyImageLoader extends HTMLElement {
     this._spinner.className = 'loader';
     this._spinner.style.display = 'block';
 
-    const indicatorColor = this.getAttribute('indicator-color');
+    const indicatorColor = this.getAttribute('indicator-color'); // indicator-color 속성 값으로 전달되는 값을 가져옵니다.
     if (indicatorColor) {
       this._spinner.style.borderTopColor = indicatorColor;
     }
-    this.applyBlur = this.hasAttribute('apply-blur');
+    this.applyBlur = this.hasAttribute('apply-blur'); // apply-blur 속성 값을 가지고 옵니다.
 
     this.observer = new IntersectionObserver(this.handleIntersection.bind(this), {
       rootMargin: '0px 0px 200px 0px',
@@ -20,11 +20,11 @@ class LazyImageLoader extends HTMLElement {
     });
   }
 
-  static get observedAttributes() {
+  static get observedAttributes() { // attributeChangedCallback에서 참조할 수 있는 속성 값을 선언합니다.
     return ['border-radius'];
   }
 
-  attributeChangedCallback(name, oldValue, newValue) {
+  attributeChangedCallback(name, oldValue, newValue) { // 변경되는 속성 값을 관찰하고 있다가 UI 업데이트에 필요한 로직을 수행합니다.
     if (name === 'border-radius') {
       this._image.style.borderRadius = newValue;
     }
@@ -34,8 +34,8 @@ class LazyImageLoader extends HTMLElement {
     return this.observer;
   }
 
-  connectedCallback() {
-    const shadowRoot = this.attachShadow({ mode: "closed" });
+  connectedCallback() { // 커스텀 엘리먼트가 DOM에 추가될 때 실행됩니다.
+    const shadowRoot = this.attachShadow({ mode: "closed" }); // 쉐도우 돔을 적용하고 외부 자바스크립트에서 쉐도우 돔 트리 내부에 있는 엘리먼트에 접근하지 못하게 합니다.
     const style = document.createElement('style');
     style.textContent = `
       .loader {
@@ -52,13 +52,13 @@ class LazyImageLoader extends HTMLElement {
         100% { transform: rotate(360deg); }
       }
     `;
-    shadowRoot.appendChild(style);
+    shadowRoot.appendChild(style); // 쉐도우 돔 트리에만 적용되어야 할 엘리먼트들을 주입합니다.
     shadowRoot.appendChild(this._spinner);
     shadowRoot.appendChild(this._image);
     this.observer.observe(this);
   }
 
-  disconnectedCallback() {
+  disconnectedCallback() { // 돔에서 엘리먼트가 삭제될 때 실행됩니다.
     this.observer.unobserve(this);
   }
 
@@ -71,8 +71,8 @@ class LazyImageLoader extends HTMLElement {
     });
   }
 
-  loadImage() {
-    const networkSpeed = this.getAttribute('data-network-speed');
+  loadImage() { 
+    const networkSpeed = this.getAttribute('data-network-speed'); // 속성 값으로 전달되는 네트워크 속도에 따라 이미지를 빠르게 혹은 느리게 다운로드 합니다.
     let delay = 0;
     if (networkSpeed === 'slow3g') {
       delay = 2000;
@@ -89,14 +89,16 @@ class LazyImageLoader extends HTMLElement {
       this._image.onload = () => {
         this._image.style.opacity = '1';
         this.style.filter = 'blur(0)';
+        this._spinner.style.display = 'none';
       };
-    }, delay);
+    }, delay); // delay만큼의 ms가 지난 후 
   }
 
-  reset() {
+  reset() { // 외부 스크립트에서도 쉐도우 돔 트리에 있는 엘리먼트의 스타일을 변경할 수 있게 메소드를 제공합니다.
     this._image.src = '';
     this._image.style.opacity = '0';
     this._image.style.display = 'block';
+    this._spinner.style.display = 'block';
   }
 }
 
