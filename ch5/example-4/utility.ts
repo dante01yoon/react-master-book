57c75409071d08ffbd7a2c3dde37302f92c7af04
dante <dante@danteui-MacBookAir.local>
2025-02-05 23:00:50 +0900

    feat: add Required T

--- ch5/example-4/utility.ts
+++ ch5/example-4/utility.ts
@@ -20,3 +20,17 @@ const readonlyUser: ReadOnlyUser = {
 
 readonlyUser.email = "dante@example.com"; // ❌ 읽기 전용 필드는 수정할 수 없습니다.
 
+
+type CompleteUserInput = Required<UpdateUserInput>; // UpdateUserInput는 모든 필드가 선택적 필드였습니다. Required<T>를 사용헀기에 선택적 필드가 모두 필수 필드로 변환됩니다.
+
+const completeUserInput: CompleteUserInput = {
+  id: "1",
+  name: "Dante",
+  email: "dante@example.com",
+};
+
+// ❌ 아래 코드는 일부 필드가 누락되어 오류가 발생합니다.
+// const invalidUser: CompleteUserInput = {
+//   name: "Dante",
+// };
+
